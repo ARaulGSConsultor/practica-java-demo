2c9c26a3a72ab61e83f966a01d60c1896d973619
angelraulgs <angelraulgs@gmail.com>
2024-05-16 23:17:42 -0600

    versionamiento de t

---
+++ src/main/java/com/java/practica/PracticaJavaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.java.practica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaJavaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaJavaDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/practica/PracticaJavaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.practica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaJavaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
